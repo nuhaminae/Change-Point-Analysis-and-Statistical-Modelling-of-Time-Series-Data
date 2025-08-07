f06e6c0bb21903a82b6e29046a3cdad7ae3b9501
Nuhamin Alemayehu <nuhaminae@gmail.com>
2025-08-07 21:05:46 +0300

    init: brent oil price dashboard scripts

--- brent-oil-price-dashboard/src/App.js
+++ brent-oil-price-dashboard/src/App.js
@@ -3,7 +3,11 @@
 import React, { useState } from 'react';
 import './App.css';
 import PriceChart from './components/PriceChart';
+import RollingMeanChart from './components/RollingMeanChart';
+import RollingStdChart from './components/RollingStdChart';
+import LogReturnChart from './components/LogReturnChart';
 import PosteriorSummary from './components/PosteriorSummary';
+import ChangePointChart from './components/ChangePointChart';
 import { Box, Typography, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 
 function App() {
@@ -15,15 +19,19 @@ function App() {
         <Typography variant="h4" gutterBottom>
           Brent Oil Price Dashboard
         </Typography>
-        <FormControl sx={{ minWidth: 200 }}>
-          <InputLabel id="view-select-label">Select View</InputLabel>
+        <FormControl sx={{ minWidth: 250 }}>
+          <InputLabel id="view-select-label">Select Chart View</InputLabel>
           <Select
             labelId="view-select-label"
             value={view}
-            label="Select View"
+            label="Select Chart View"
             onChange={(e) => setView(e.target.value)}
           >
             <MenuItem value="price">Price Chart</MenuItem>
+            <MenuItem value="rollingMean">Rolling Mean</MenuItem>
+            <MenuItem value="rollingStd">Rolling Std Dev</MenuItem>
+            <MenuItem value="logReturn">Log Return</MenuItem>
+            <MenuItem value="changePoints">Change Point Chart</MenuItem>
             <MenuItem value="posterior">Posterior Summary</MenuItem>
           </Select>
         </FormControl>
@@ -32,12 +40,13 @@ function App() {
       <main>
         <Box sx={{ mt: 4 }}>
           {view === 'price' && <PriceChart />}
+          {view === 'rollingMean' && <RollingMeanChart />}
+          {view === 'rollingStd' && <RollingStdChart />}
+          {view === 'logReturn' && <LogReturnChart />}
+          {view === 'changePoints' && <ChangePointChart />}
           {view === 'posterior' && <PosteriorSummary />}
         </Box>
       </main>
     </div>
   );
-}
-
-export default App;
-
+} export default App;
